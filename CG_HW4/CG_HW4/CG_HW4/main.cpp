770e69ddd1bb2e1b05765363cec67200873b8ea9
ChihM <s102062111@m102.nthu.edu.tw>
2015-05-29 13:05:44 +0800

    add hw4 lighting

--- CG_HW4/CG_HW4/CG_HW4/main.cpp
+++ CG_HW4/CG_HW4/CG_HW4/main.cpp
@@ -65,6 +65,7 @@ GLuint texNum[MAX_TEXTURE_NUM];
 unsigned char image[MAX_TEXTURE_NUM][MAXSIZE]; //image data array 
 FileHeader FH[MAX_TEXTURE_NUM]; //BMP FileHeader 
 InfoHeader IH[MAX_TEXTURE_NUM]; //BMP InfoHeader
+
 int isTextureMapping = 0;
 GLint iLocTexCoord;
 GLint texture_wrap_mode = GL_REPEAT;
@@ -112,6 +113,26 @@ GLint iLocPosition;
 GLint iLocColour;
 GLint iLocMVP;
 
+GLint iLocNormal;
+GLint iLocMDiffuse;
+GLint iLocMAmbient;
+GLint iLocMSpecular;
+GLint iLocMShiness;
+/***** May be I need to use this iLoc ******/
+GLint iLocLAmbient; 
+GLint iLocLDiffuse;
+GLint iLocLSpecular;
+GLint iLocLConsAtt;
+GLint iLocLLintAtt;
+GLint iLocLPosition;
+GLint iLocLSpotDir;
+GLint iLocLSpotCosCutOff;
+GLint iLocLSpotExp;
+
+/*******************************************/
+
+
+
 float rotateDeltaX, rotateDeltaY;
 float rotateStartX, rotateStartY;
 float rotateX, rotateY;
@@ -133,6 +154,13 @@ float aMVP[16];
 GLMmodel* OBJ;
 GLfloat normalScale;
 GLfloat scale;
+
+
+/****** LIGHTING ARGUMENT ******/
+GLfloat p_x = 0, p_y = 0, p_z = 5;
+GLfloat s_x = 0, s_y = 0, s_z = -1;
+GLfloat s_cos = 0.5;
+/******************************/
 
 // unpack bmp file
 void LoadTextures(char* filename, int index)
@@ -183,6 +211,55 @@ void setTexture()
 	glGenTextures(tex_count, texNum);
 	for(int i = 0; i < tex_count; i++){
 		initTextures(i);
+	}
+}
+
+void NormalModel(){
+	GLMgroup* group = OBJ->groups;
+	int gCount = 0;
+	while(group){
+		for(unsigned int i=0; i<group->numtriangles; i++){
+			// triangle index
+			int triangleID = group->triangles[i];
+
+			// the index of each vertex
+			int indv1 = OBJ->triangles[triangleID].nindices[0];
+			int indv2 = OBJ->triangles[triangleID].nindices[1];
+			int indv3 = OBJ->triangles[triangleID].nindices[2];
+
+			// vertices
+			GLfloat vx, vy, vz;
+			
+
+			vx = OBJ->normals[indv1*3];
+			vy = OBJ->normals[indv1*3+1];
+			vz = OBJ->normals[indv1*3+2];
+			
+			//printf("%f %f %f\n", vx, vy, vz);
+			vnormals[gCount][i*9+0] = vx;
+			vnormals[gCount][i*9+1] = vy;
+			vnormals[gCount][i*9+2] = vz;
+			//printf("%f %f %f\n",vertices[i*9], vertices[i*9+1], vertices[i*9+2]);
+			vx = OBJ->normals[indv2*3];
+			vy = OBJ->normals[indv2*3+1];
+			vz = OBJ->normals[indv2*3+2];
+			//printf("%f %f %f\n", vx, vy, vz);
+			vnormals[gCount][i*9+3] = vx;
+			vnormals[gCount][i*9+4] = vy;
+			vnormals[gCount][i*9+5] = vz;
+			//printf("%f %f %f\n",vertices[i*9+3], vertices[i*9+4], vertices[i*9+5]);
+			vx = OBJ->normals[indv3*3];
+			vy = OBJ->normals[indv3*3+1];
+			vz = OBJ->normals[indv3*3+2];
+			//printf("%f %f %f\n", vx, vy, vz);
+			vnormals[gCount][i*9+6] = vx;
+			vnormals[gCount][i*9+7] = vy;
+			vnormals[gCount][i*9+8] = vz;
+			//printf("%f %f %f\n",vertices[i*9+6], vertices[i*9+7], vertices[i*9+8]);
+			//printf("\n");
+		}
+		group = group->next;
+		gCount++;
 	}
 }
 
@@ -285,11 +362,17 @@ void loadOBJModel(int index)
 
 	// uncomment them only if there is no vertex normal info in your .obj file
 	// glm will calculate the vertex normals for you
-//	glmFacetNormals(OBJ);         
+//	glmFacetNormals(OBJ); 
+/*
+	int indv1 = OBJ->triangles[0].nindices[0];
+	if(indv1 == 3452816845) 
+		glmVertexNormals(OBJ, 90.0);
+*/
 //	glmVertexNormals(OBJ, 90.0);  
 
 	// parse texture model and align the vertices
 	TextureModel();
+	NormalModel();
 }
 
 void setViewPortMatrix()
@@ -588,10 +671,16 @@ void renderScene(void)
 	}
 
 	Matrix4 modelMatrix;
-	// Model matrix ends here
+	// Model matrix ends here (transformation matrix)
+	/***** ORGIN *******
 	for(int i=0;i<16;i++){
 		modelMatrix[i] = M[i];
 	}
+	***** ORGIN *******/
+	for(int i=0;i<16;i++){
+		modelMatrix[i] = M[i];
+	}
+
 
 	// V (lookat)
 	M = M * myLookAtMatrix(eyeVec[0],eyeVec[1],eyeVec[2],  eyeVec[0],eyeVec[1],eyeVec[2]-1.0f,  0,1,0);
@@ -623,15 +712,52 @@ void renderScene(void)
 	glUniformMatrix4fv(iLocMVP, 1, GL_FALSE, M.get());
 	glUniformMatrix4fv(iLocModelMatrix, 1, GL_FALSE, modelMatrix.get());
 
+
+	// lighting
+	glUniform4f (iLocLPosition, p_x, p_y, p_z, 0);
+	
+	glUniform3f(iLocLSpotDir, s_x, s_y, s_z);
+	glUniform1f(iLocLSpotCosCutOff, s_cos);  // here is spot light !!
+
+
+	glUniform4f(iLocLAmbient, 0.5, 0.5, 0.5, 1);
+	glUniform4f (iLocLDiffuse, 0.8f, 0.8f, 0.8f, 0.8f);
+	glUniform4f (iLocLSpecular, 1.0f, 1.0f, 1.0f, 1);
+
 	GLMgroup* group = OBJ->groups;
 	int gCount = 0;
 	while(group){
 		// enable attributes array
 		glEnableVertexAttribArray(iLocPosition);
+		glEnableVertexAttribArray(iLocNormal);
 		// TODO: texture VertexAttribArray is enabled here
 
 		// bind attributes array
 		glVertexAttribPointer(iLocPosition, 3, GL_FLOAT, GL_FALSE, 0, vertices[gCount]);
+		glVertexAttribPointer(iLocNormal, 3, GL_FLOAT, GL_FALSE, 0, vnormals[gCount]);
+
+		glUniform1f( iLocMShiness, OBJ->materials[group->material].shininess);
+		//printf("shiness = %f\n", OBJ->materials[group->material].shininess);
+		glUniform4f (iLocMAmbient,	OBJ->materials[group->material].ambient[0], 
+									OBJ->materials[group->material].ambient[1],
+									OBJ->materials[group->material].ambient[2],
+									OBJ->materials[group->material].ambient[3]
+					);
+		
+		glUniform4f (iLocMDiffuse,	OBJ->materials[group->material].diffuse[0], 
+									OBJ->materials[group->material].diffuse[1],
+									OBJ->materials[group->material].diffuse[2],
+									OBJ->materials[group->material].diffuse[3]
+					);
+		
+	
+		glUniform4f (iLocMSpecular,	OBJ->materials[group->material].specular[0], 
+									OBJ->materials[group->material].specular[1],
+									OBJ->materials[group->material].specular[2],
+									OBJ->materials[group->material].specular[3]
+					);
+
+
 		// TODO: bind texture vertex attribute pointer here
 
 		// texture mag/min filter
@@ -674,9 +800,9 @@ void showHelp()
 	printf("                   press X / x to switch next model.\n");
 	printf("\n");
 }
-
-void processSpecialKeys(int key, int x, int y){
 	/*
+void processSpecialKeys(int key, int x, int y){
+
 	GLUT_KEY_F1			F1 function key.
 	GLUT_KEY_F2			F2 function key.
 	GLUT_KEY_F3			F3 function key.
@@ -698,9 +824,9 @@ void processSpecialKeys(int key, int x, int y){
 	GLUT_KEY_HOME		Home directional key.
 	GLUT_KEY_END		End directional key.
 	GLUT_KEY_INSERT		Inset directional key.
-	*/
+	
 }
-
+*/
 void processNormalKeys(unsigned char key, int x, int y) {
 	int modifier = glutGetModifiers();
 	//printf("modifier = %d\n", modifier);
@@ -749,6 +875,14 @@ void processNormalKeys(unsigned char key, int x, int y) {
 				projectionMode = PROJECTION_PARA;
 				printf("parallel projection\n");
 			}
+			break;
+		
+		case '+':
+			s_cos -= 0.1;
+			break;
+		
+		case '-':
+			s_cos += 0.1;
 			break;
 
 		case 27: // esc key
@@ -827,8 +961,24 @@ void setShaders() {
 	iLocPosition = glGetAttribLocation(p, "av4position");
 	iLocColour   = glGetAttribLocation(p, "av3colour");
 	iLocTexCoord = glGetAttribLocation(p, "av2texCoord");
-
+	iLocNormal = glGetAttribLocation(p, "av3normal");
 	iLocMVP = glGetUniformLocation(p, "mvp");
+
+	iLocMAmbient = glGetUniformLocation(p, "Material.ambient");
+	iLocMDiffuse = glGetUniformLocation(p, "Material.diffuse");
+	iLocMShiness = glGetUniformLocation(p, "Material.shiniess");
+	iLocMSpecular = glGetUniformLocation(p, "Material.specular");
+
+	iLocLAmbient = glGetUniformLocation(p, "LightSource.ambient");
+	iLocLDiffuse = glGetUniformLocation(p, "LightSource.diffuse"); 
+	iLocLSpecular = glGetUniformLocation(p, "LightSource.specular");
+	iLocLConsAtt = glGetUniformLocation(p, "LightSource.constantAtteenuation");
+	iLocLLintAtt =  glGetUniformLocation(p, "LightSource.linearAtteenuation");
+	iLocLPosition = glGetUniformLocation(p, "LightSource.position");
+	iLocLSpotDir = glGetUniformLocation(p, "LightSource.spotDirection");
+	iLocLSpotCosCutOff = glGetUniformLocation(p, "LightSource.spotCosCutoff");
+	iLocLSpotExp = glGetUniformLocation(p, "LightSource.spotExponent");
+
 
 	iLocIsTextureMapping = glGetUniformLocation(p, "uiisTextureMapping");
 	iLocModelMatrix  = glGetUniformLocation(p, "um4modelMatrix");
@@ -889,6 +1039,24 @@ void myMotionFunc(int x, int y)
 		translateStartY = (float)y;
 	}
 	isMouseMoving = 1;
+}
+
+void processSpecialKeys(int key, int x, int y){
+	switch (key) {
+		case GLUT_KEY_LEFT:
+			p_x -= 0.1;
+			break;
+		case GLUT_KEY_RIGHT:
+			p_x += 0.1;
+			break;
+		case GLUT_KEY_UP:
+			p_y += 0.1;
+			break;
+		case GLUT_KEY_DOWN:
+			p_y -= 0.1;
+			break;
+
+	}
 }
 
 int main(int argc, char **argv)
--- CG_HW4/CG_HW4/CG_HW4/shader.frag
+++ CG_HW4/CG_HW4/CG_HW4/shader.frag
@@ -3,10 +3,13 @@
 //uniform int uiisTextureMapping;
 
 //varying vec2 v2texCoord;
-varying vec4 vv4colour, vv4vertice;
+varying vec4 vv4color, vv4vertice;
 
 void main() {
-	gl_FragColor = vec4(vv4vertice.zzz/2.0/1.732+0.5, 0.0);
+	//gl_FragColor = vec4(vv4vertice.zzz/2.0/1.732+0.5, 0.0);
+	
+	gl_FragColor = vv4color;
+	
 	/*
 	if(uiisTextureMapping != 0){
 		vec4 texColor = texture2D(ustexture, v2texCoord);
